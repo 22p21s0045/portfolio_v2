b1f0044a1bbcc9aafcf429e3ce3f3c94b4dfc2a0
lommel <monkeytitle@hotmail.com>
2022-05-07 19:34:15 +0700

    [ADD]-spline

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,21 +6,26 @@ import Navbar from "../components/Navbar";
 import Grid from "@mui/material/Grid";
 import Typewriter from "typewriter-effect";
 import Box from "@mui/material/Box";
+import Welcome from "../components/Welcome";
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Navbar />
-      <Grid container sx={{marginTop:20,marginLeft:14}}>
+      <Grid container sx={{ marginTop: 20, marginLeft: 14 }}>
         <Grid item xs={6}>
-          <Box sx={{fontFamily:"Boogaloo",fontSize:76}}>
+          <Box sx={{ fontFamily: "Boogaloo", fontSize: 76 }}>
             <Typewriter
               onInit={(typewriter) => {
                 typewriter.typeString("Welcome to my place").start();
               }}
             />
           </Box>
+          <Welcome /> 
         </Grid>
-        <Grid item xs={6}></Grid>
+        <Grid item xs={6}>
+          
+          
+        </Grid>
       </Grid>
     </div>
   );
