7206c0b11aa864072dc6cc85197c741cf6d8d0c8
lommel <monkeytitle@hotmail.com>
2022-05-08 12:49:37 +0700

    [ADD]-animation-welcome-success

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -7,7 +7,7 @@ const Spline = dynamic(() => import('@splinetool/react-spline'), {
 export default function App() {
   return (
     <div>
-      <Spline scene="https://prod.spline.design/7tU2e6C1lxINwmuv/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/YdNXPKPis5ZEIXqR/scene.splinecode" />
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -7,12 +8,19 @@ import Grid from "@mui/material/Grid";
 import Typewriter from "typewriter-effect";
 import Box from "@mui/material/Box";
 import Welcome from "../components/Welcome";
+import { useEffect,useState } from "react";
 const Home: NextPage = () => {
+  
   return (
     <div className={styles.container}>
       <Navbar />
+      <Typewriter
+              onInit={(typewriter) => {
+                typewriter.typeString("Welcome to my place").start();
+              }}
+            />
       <Grid container sx={{ marginTop: 20 }}>
-        <Grid item xs={6} sx={{marginLeft: 14}}>
+        <Grid item xs={6} sx={{ marginLeft: 14 }}>
           <Box sx={{ fontFamily: "Boogaloo", fontSize: 76 }}>
             <Typewriter
               onInit={(typewriter) => {
@@ -20,12 +28,32 @@ const Home: NextPage = () => {
               }}
             />
           </Box>
-          <Welcome /> 
         </Grid>
         <Grid item xs={6}>
-          
-          
+          <Box sx={{ marginTop: 0 }}>
+            <Welcome />
+          </Box>
         </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item xs={12}>
+          <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          </Grid>
+
       </Grid>
     </div>
   );
