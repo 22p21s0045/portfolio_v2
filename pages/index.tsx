2a2a5af05a1de4f3561dda2058a22dc85f59a479
lommel <monkeytitle@hotmail.com>
2022-05-09 23:54:42 +0700

    [ADD]-about-me

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,16 @@ import Navbar from "../components/Navbar";
 import Grid from "@mui/material/Grid";
 import Typewriter from "typewriter-effect";
 import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Stack from '@mui/material/Stack';
 import Droll from "../styles/image/droll.svg";
 import Welcome from "../components/Welcome";
 import Room from "../components/Room";
-import Line from "../styles/svg/line.svg";
-import SunIcon from "../components/Line";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { motion, useAnimation } from "framer-motion";
-import { ParallaxProvider } from 'react-scroll-parallax';
-import { Parallax } from 'react-scroll-parallax';
+import { ParallaxProvider } from "react-scroll-parallax";
+import { Parallax } from "react-scroll-parallax";
 const Home: NextPage = () => {
   const animation = useAnimation();
   const { ref, inView } = useInView();
@@ -39,92 +39,111 @@ const Home: NextPage = () => {
   return (
     <div>
       <ParallaxProvider>
-      <Navbar />
+        <Navbar />
 
-      <Grid container sx={{ marginTop: 20 }}>
-        <Grid item xs={6} sx={{ marginLeft: 14 }}>
-          <Box sx={{ fontFamily: "Boogaloo", fontSize: 76 }}>
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter.typeString("Welcome to my place").start();
-              }}
-            />
-          </Box>
-        </Grid>
-        <Grid item xs={6}>
-          <Box sx={{ marginTop: 0 }}>
-            <Welcome />
-          </Box>
-        </Grid>
-      </Grid>
-      <Grid container sx={{ backgroundColor: "#50CB93" }} alignItems="center" justifyContent="center">
-        <Grid item lg={6} xs={12} md={5}>
-          <Room /> 
-        </Grid>
-       
-        <Grid item lg={6} xs={12} md ={6}>
-        <Parallax translateY={[20, -40]}>
-          <motion.div animate={animation} ref={ref}>
-            <Box
-              sx={{
-                marginLeft: "10%",
-                marginTop: 10,
-                transform: "rotate(15deg)",
-              }}
-              className="Hero"
-            >
-              <Image
-                src="/hero.jpg"
-                width={600}
-                height={685}
-                className="Hero"
+        <Grid container sx={{ marginTop: 20 }}>
+          <Grid item xs={6} sx={{ marginLeft: 14 }}>
+            <Box sx={{ fontFamily: "Boogaloo", fontSize: 76 }}>
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter.typeString("Welcome to my place").start();
+                }}
               />
-              <Box className="stats">
-                <Box sx={{ position: "relative", marginLeft: "10%" }}>
-                  <h1 style={{ fontFamily: "Boogaloo" }}>Coding</h1>
-
-                  <ProgressBar
-                    completed={30}
-                    customLabel=" "
-                    bgColor="#FF3D68"
-                    width="50%"
-                    labelColor="#e80909"
-                    maxCompleted={100}
-                  />
-                </Box>
-                <Box sx={{ position: "relative", marginLeft: "10%" }}>
-                  <h1 style={{ fontFamily: "Boogaloo" }}>Design</h1>
-
-                  <ProgressBar
-                    completed={40}
-                    customLabel=" "
-                    bgColor="#A73489"
-                    width="50%"
-                    labelColor="#e80909"
-                    maxCompleted={100}
-                  />
-                </Box>
-                <Box sx={{ position: "relative", marginLeft: "10%" }}>
-                  <h1 style={{ fontFamily: "Boogaloo" }}>Business</h1>
-
-                  <ProgressBar
-                    completed={15}
-                    customLabel=" "
-                    bgColor="#FAAD80"
-                    width="50%"
-                    labelColor="#e80909"
-                    maxCompleted={100}
-                  />
-                </Box>
-                <Image src={Droll} width={377} height={350} />
-              </Box>
             </Box>
-           
-          </motion.div>
-          </Parallax>
+          </Grid>
+          <Grid item xs={6}>
+            <Box sx={{ marginTop: 0 }}>
+              <Welcome />
+            </Box>
+          </Grid>
         </Grid>
-        <SunIcon/>
-      </Grid>
+        <Grid
+          container
+          sx={{ backgroundColor: "#50CB93" }}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Grid item lg={6} xs={12} md={5}>
+            <Parallax translateY={[20, -40]}>
+              <Room />
+            </Parallax>
+          </Grid>
+
+          <Grid item lg={6} xs={12} md={6}>
+            <Parallax translateY={[20, -40]}>
+              <motion.div animate={animation} ref={ref}>
+                <Box
+                  sx={{
+                    marginLeft: "10%",
+                    marginTop: 10,
+                    transform: "rotate(15deg)",
+                  }}
+                  className="Hero"
+                >
+                  <Image
+                    src="/hero.jpg"
+                    width={600}
+                    height={685}
+                    className="Hero"
+                  />
+                  <Box className="stats">
+                    <Box sx={{ position: "relative", marginLeft: "10%" }}>
+                      <h1 style={{ fontFamily: "Boogaloo" }}>Coding</h1>
+
+                      <ProgressBar
+                        completed={30}
+                        customLabel=" "
+                        bgColor="#FF3D68"
+                        width="50%"
+                        labelColor="#e80909"
+                        maxCompleted={100}
+                      />
+                    </Box>
+                    <Box sx={{ position: "relative", marginLeft: "10%" }}>
+                      <h1 style={{ fontFamily: "Boogaloo" }}>Design</h1>
+
+                      <ProgressBar
+                        completed={40}
+                        customLabel=" "
+                        bgColor="#A73489"
+                        width="50%"
+                        labelColor="#e80909"
+                        maxCompleted={100}
+                      />
+                    </Box>
+                    <Box sx={{ position: "relative", marginLeft: "10%" }}>
+                      <h1 style={{ fontFamily: "Boogaloo" }}>Business</h1>
+
+                      <ProgressBar
+                        completed={15}
+                        customLabel=" "
+                        bgColor="#FAAD80"
+                        width="50%"
+                        labelColor="#e80909"
+                        maxCompleted={100}
+                      />
+                    </Box>
+                    <Image src={Droll} width={377} height={350} />
+                  </Box>
+                </Box>
+              </motion.div>
+            </Parallax>
+          </Grid>
+        </Grid>
+        <Grid container sx={{ backgroundColor: "#ACFFAD" }}>
+          <Grid item xs={5}>
+            <Paper>
+              <Stack sx={{fontFamily: "Boogaloo",maringLeft: 10,position:"relative" }}>
+                <h1>About Me</h1>
+                <h1>Name: Akkarawit Sittiprakan</h1>
+                <h1>Age: 18 years old</h1>
+                <h1> Education: Informaiton technology KMUTT </h1>
+                <h1> Skills : Programming,Design,Analytic </h1>
+               
+                </Stack>
+            </Paper>
+          </Grid>
+        </Grid>
       </ParallaxProvider>
     </div>
   );
