fb36189f7749a7984933574dc23319b8717a47b3
lommel <monkeytitle@hotmail.com>
2022-05-11 22:29:10 +0700

    [ADD]-get-static-props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import Head from "next/head";
+import { InferGetStaticPropsType,GetStaticProps } from 'next'
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import ProgressBar from "@ramonak/react-progress-bar";
@@ -21,7 +23,8 @@ import { useInView } from "react-intersection-observer";
 import { motion, useAnimation } from "framer-motion";
 import { ParallaxProvider } from "react-scroll-parallax";
 import { Parallax } from "react-scroll-parallax";
-const Home: NextPage = () => {
+const Home: NextPage = ({data}:InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(data.data);
   const animation = useAnimation();
   const { ref, inView } = useInView();
   useEffect(() => {
@@ -212,19 +215,25 @@ const Home: NextPage = () => {
                     spacing={5}
                     justifyContent="center"
                   >
-                    <Paper className="Skill-box">
-                      <Box className="floating-img">
-                        <Image
-                          src={require("../styles/icon/python.png")}
-                          width={100}
-                          height={100}
-                        />
-                      </Box>
-                      <Box className="Skill-tag">
-                      <h1 style={{ fontFamily: "Boogaloo" }}>Beginner</h1>
-                        <h1 style={{ fontFamily: "Boogaloo" }}>Python</h1>
-                      </Box>
-                    </Paper>
+                    {data.data.skills.data.map((item) => {
+                      return (
+                        <Paper className="Skill-box">
+                        <Box className="floating-img">
+                          <Image
+                            src={require("../styles/icon/python.png")}
+                            width={100}
+                            height={100}
+                          />
+                        </Box>
+                        <Box className="Skill-tag">
+                        <h1 style={{ fontFamily: "Boogaloo" }}>Beginner</h1>
+                          <h1 style={{ fontFamily: "Boogaloo" }}>Python</h1>
+                        </Box>
+                      </Paper>
+                      );
+
+                    })}
+                   
                     <Paper>
                       <Image
                         src={require("../styles/icon/python.png")}
@@ -258,5 +267,39 @@ const Home: NextPage = () => {
     </div>
   );
 };
+export async function getStaticProps():GetStaticProps {
+  const client = new ApolloClient({
+    uri: "http://localhost:1337/graphql",
+    cache: new InMemoryCache(),
+  });
+  const { data } = await client.query(
+    {
+      query:gql`
+      query Getskill{
+  skills{
+    data{
+      attributes{
+        Name
+        tag
+        Icon{
+          data{
+            attributes{
+              url
+            }
+          }
+          }
+        }
+        
+      }
+    }
+  
+  }`,
+})
+  return {
+    props: {
+      data: data,
+    },
+  };
+}
 
 export default Home;
